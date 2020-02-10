f8b83374d00421f2bef31071c340198befa89260
Paniov-M <58479393+Paniov-M@users.noreply.github.com>
2020-02-10 14:31:32 +0200

    Update Draw.h

--- Draw.h
+++ Draw.h
@@ -1,1 +1,57 @@
 #pragma once
+#pragma once
+#include <iostream>
+#include "Source.cpp"
+void Draw()                                                                                               //painting  the area
+{
+
+	system("cls");                                       //this thing will be clean our monitor after every call functions Draw();
+
+	for (int i = 0;i < width + 1;i++)
+		cout << "#";
+	cout << endl;
+
+	for (int i = 0; i < height;i++)
+	{
+		for (int j = 0; j < width;j++)
+		{
+			if (j == 0 || j == width - 1)                          //first and last cell will be "#" and cell beetwen them will be empty;
+
+				cout << "#";
+
+			if (i == y && j == x)                                    //it is a head of our snake; 
+
+				cout << "0";
+
+
+			else if (i == fruitY && j == fruitX)              //it is our fruit;
+				cout << "F";
+
+			else
+			{
+				bool print = false;
+				for (int k = 0; k < nTail; k++)
+				{
+					if (tailX[k] == j && tailY[k] == i)
+					{
+						print = true;
+						cout << "o";
+
+					}
+				}
+
+				if (!print)
+					cout << " ";
+			}
+
+		}
+
+		cout << endl;
+
+	}
+
+	for (int i = 0;i < width + 1;i++)
+		cout << "#";
+	cout << endl;
+	cout << "SCORE - " << score << endl;
+}
