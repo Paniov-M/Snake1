db9a394436c5c562b28cb09a0e4aac7059aae21b
Paniov-M <58479393+Paniov-M@users.noreply.github.com>
2020-02-10 14:33:46 +0200

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,1 +1,47 @@
 #pragma once
+#include <iostream>
+#include "Source.cpp"
+#include <iostream>
+#include <conio.h>          //it is for the Input to see what player press;
+#include <iomanip>
+#include <windows.h>
+#include <fstream>
+#include <stdio.h>     
+using namespace std;
+
+bool gameOver;                                                                                             //it is varie for the end of game;
+
+const int width = 20;                                                                                      //width - shuruna;
+
+const int height = 20;
+
+int x, y;                                                                                  //coordinates our snakes;
+
+int fruitX, fruitY;                                                                 //coordinates our fruit;
+
+int tailX[100], tailY[100];
+
+int nTail;
+
+int score;                                                                         //it is our list of point;
+
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };                                     //for the move of snake;
+
+eDirection dir;
+
+
+
+int main()
+{
+	Setup();
+
+	while (!gameOver)
+	{
+		Draw();
+		Input();
+		Logik();
+
+	}
+
+	return(0);
+}
