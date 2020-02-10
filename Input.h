1bfb34dca0d325b58942a294c7eaf514858bc571
Paniov-M <58479393+Paniov-M@users.noreply.github.com>
2020-02-10 14:32:44 +0200

    Update Input.h

--- Input.h
+++ Input.h
@@ -1,1 +1,31 @@
 #pragma once
+#include <iostream>
+#include "Source.cpp"
+void Input()                                                                                         //when playes press the button 
+{
+	if (_kbhit())                     //to see what player press and search this button;
+	{
+		switch (_getch())            //our move; 
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+
+		case 'd':
+			dir = RIGHT;
+			break;
+
+		case 's':
+			dir = DOWN;
+			break;
+
+		case 'w':
+			dir = UP;
+			break;
+
+		case 'x':
+			gameOver = true;         //for ending  the game;
+			break;
+		}
+	}
+}
